b90ee915b1c4b0e500a3a7d969630d0cf5ee0806
trujilml <mickeytrj@gmail.com>
2021-11-09 19:47:52 -0600

    created html file using node fs write file method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
-
+const fs = require('fs');
 
 const generatePage = (name, github) => {
 return `
@@ -21,12 +21,8 @@ return `
 };
 
 
-// const generatePage = (userName, githubName) => {
-// return `
-// Name: ${userName}
-// GitHub: ${githubName}
-// `;
-// };
-
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw err;
+    
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
