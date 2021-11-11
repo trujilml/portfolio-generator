41317d91d0216787995d152e25fbd3c8c6087454
trujilml <mickeytrj@gmail.com>
2021-11-11 00:52:14 -0600

    added ability to generate prompt to add more projects to portfolio generator

--- app.js
+++ app.js
@@ -20,12 +20,18 @@ const promptUser = () => {
     ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
     console.log(`
 =================
 Add a New Project
 =================
 `);
+
+//if there is no 'projects' array property, create one
+if (!portfolioData.projects) {
+portfolioData.projects = [];
+}
+
     return inquirer.prompt([
         {
             type: 'input',
@@ -60,13 +66,22 @@ Add a New Project
             message: 'Would you like to enter another project?',
             default: false
         }
-    ]);
+    ])
+    .then(projectData => {portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+    
 };
 
 promptUser()
-.then(answers => console.log(answers))
 .then(promptProject)
-.then(projectAnswers => console.log(projectAnswers));
+.then(portfolioData => {
+    console.log(portfolioData);
+});
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
