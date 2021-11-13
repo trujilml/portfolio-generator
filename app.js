5846f685e4d9c62a5e10f2eda850871340bd517e
trujilml <mickeytrj@gmail.com>
2021-11-13 16:55:00 -0600

    began establishing promises for the write file and call file function to print out completed index html file for generated portfolio

--- app.js
+++ app.js
@@ -149,27 +149,3 @@ promptUser()
 .catch(err => {
     console.log(err);
 });
-
-
-
-const pageHTML = generatePage(portfolioData);
-
-
-fs.writeFile('./dist/index.html', pageHTML, err => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-
-    console.log('Page created! Check out index.html in this directory to see it!');
-
-    fs.copyFile('./src/style.css', './dist/style.css', err => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-
-    console.log('Style sheet copied!');
-    });
-    });
-});
